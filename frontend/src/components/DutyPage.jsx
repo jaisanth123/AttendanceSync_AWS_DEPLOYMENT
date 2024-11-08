629fb36bcd6e2968a349455f54e4fa5b55ba02dd
jaisanth123 <jaisanth2006@gmail.com>
2024-11-08 11:58:34 +0530

    updated counter

--- frontend/src/components/DutyPage.jsx
+++ frontend/src/components/DutyPage.jsx
@@ -39,19 +39,21 @@ function DutyPage({ selectedCourse }) {
     <div className="flex flex-col items-center flex-1 p-6 md:p-8 lg:p-12">
       <div className="flex flex-col items-center mt-6 space-y-4">
         <div className="flex flex-col items-center space-x-0 space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+        <button
+            onClick={handleIncrement}
+            className="w-full px-6 py-3 text-white transition-all duration-300 bg-green-500 rounded-lg shadow-md md:w-auto hover:bg-green-600"
+          >
+            Increment
+          </button>
+
+          <span className="text-2xl font-semibold">{counter}</span>
           <button
             onClick={handleDecrement}
             className="w-full px-6 py-3 text-white transition-all duration-300 bg-red-500 rounded-lg shadow-md md:w-auto hover:bg-red-600"
           >
             Decrement
           </button>
-          <span className="text-2xl font-semibold">{counter}</span>
-          <button
-            onClick={handleIncrement}
-            className="w-full px-6 py-3 text-white transition-all duration-300 bg-green-500 rounded-lg shadow-md md:w-auto hover:bg-green-600"
-          >
-            Increment
-          </button>
+
         </div>
       </div>
 
