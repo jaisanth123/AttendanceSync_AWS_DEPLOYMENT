8ccec24119afd032ae9ee94e3b357f5c78faebab
Vijayakanth M <vikymahendiran123@gmail.com>
2024-11-22 20:57:59 +0530

    22/11 commit

--- backend/models/Attendance.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const attendanceSchema = new mongoose.Schema({
-  date: { type: Date, default: Date.now },
-  rollNo: String,
-  status: { type: String, enum: ['Present', 'Absent', 'On Duty'] },
-});
-
-module.exports = mongoose.model('Attendance', attendanceSchema);
