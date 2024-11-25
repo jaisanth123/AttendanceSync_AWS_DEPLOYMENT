4788a6759eb3c75a19b93f1ce2cfaf2b237017be
RashmikaRamkumar <krrashmika2004@gmail.com>
2024-11-25 23:06:02 +0530

    removed excel generation old 25/11

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,20 +1,11 @@
 const express = require('express');
-const {handleAbsentStudentsReport, generateAbsentStudentsMessage,handleDownloadAbsentReport,handleCustomDownloadAbsentReport } = require('../controllers/reportController');
+const { generateAbsentStudentsMessage,handleDownloadAbsentReport,handleCustomDownloadAbsentReport } = require('../controllers/reportController');
 
 const router = express.Router();
 
 // Define the route to generate the absent students' message
 router.get('/absentStudents', generateAbsentStudentsMessage);
 
-// Route for generating report for male students
-router.get('/reportabsent/male', (req, res) => {
-    handleAbsentStudentsReport('MALE', req, res);
-});
-
-// Route for generating report for female students
-router.get('/reportabsent/female', (req, res) => {
-    handleAbsentStudentsReport('FEMALE', req, res);
-});
 
 router.get('/downloadreport/male', (req, res) => {
     handleDownloadAbsentReport('MALE', req, res);
