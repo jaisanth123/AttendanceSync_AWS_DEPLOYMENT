6a486e0dd8b2b44d5f4b26972af461964fe1cd44
RashmikaRamkumar <krrashmika2004@gmail.com>
2024-11-30 10:54:21 +0530

    vercel frontend assigned to cors

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -16,6 +16,8 @@ const sendToken = (user, statusCode, res, message) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure :true,
+    sameSite : "None"
   };
 
   res.status(statusCode).json({
